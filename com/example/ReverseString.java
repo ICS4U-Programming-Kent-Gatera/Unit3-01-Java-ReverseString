776573527c23e07266c00cef421317ac5318b982
Kent Gatera <kent.gatera@stu.ocsb.ca>
2024-05-02 14:54:43 +0000

    Y

--- com/example/ReverseString.java
+++ com/example/ReverseString.java
@@ -1,28 +1,62 @@
-
-package com.example;
+import java.util.*;
+import java.io.*;
 
 /**
- * Change me.
+ * Reverses a string using a recursive function.
  *
- * @author Kent Gatera
+ * @author Kent G
  * @version 1.0
- * @since 2024-04-26
+ * @since 2024-04-10
  */
 
-// ReverseString class
-public final class ReverseString {
+class ReverseString{
+
 
     /** Private constructor to prevent instantiation. */
     private ReverseString() {
-        throw new UnsupportedOperationException("Cannot instantiate");
+      throw new UnsupportedOperationException("Utility class.");
     }
-
+  
     /**
      * This is the main method.
      *
      * @param args Unused
      */
-    public static void main(final String[] args) {
-        System.out.println("Hello, world");
+    public static void main(String[] args) {
+        File readFile = new File("input.txt");
+        try {
+            FileWriter fileWriter = new FileWriter("output.txt");
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            Scanner sc = new Scanner(readFile);
+            // Passes input lines to the string reverser then writes them to output.
+            while (sc.hasNextLine()) {
+                String inputString = sc.nextLine();
+                writer.write(stringReverse(inputString));
+                writer.newLine();
+            }
+            writer.close();
+            sc.close();
+        } catch (Exception e) {
+            System.out.println("FILE NOT FOUND");
+        }
     }
-}
+
+   /**
+   * Recursive method that reverses its input string.
+   *
+   * @param inputString passed.
+   * @return inputString.
+   */
+    private static String stringReverse(final String inputString) {
+        // Returns the reversed string once its length is zero.
+        if (inputString.length() == 0) {
+          return inputString;
+          /* Otherwise, return the input string passed into the
+           * string reverser again but with its index shifted over one
+           * added to the first character of the input string.
+           */
+        } else {
+          return stringReverse(inputString.substring(1)) + inputString.charAt(0);
+        }
+      }
+    }
